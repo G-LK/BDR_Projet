5db1ec2c3911376d09310e1acc27a67384d43232
GLK <l.a.klasen@gmail.com>
2023-11-21 14:27:58 +0100

    all from start

---
+++ Etape3/database_script.sql
@@ -0,0 +1,25 @@
+CREATE TABLE gym;
+
+CREATE TABLE gym.user (
+    name varchar() NOT NULL,
+    lastname varchar() NOT NULL,
+    email varchar() NOT NULL,
+    password varchar() NOT NULL,
+    type integer,
+    sexe char(1),
+    bday date
+
+);
+
+CREATE TABLE gym.client (
+    sportLevel integer,
+    
+)
+
+
+
+
+
+
+
+
